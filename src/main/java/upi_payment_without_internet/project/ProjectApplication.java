eaa106012fee220adb5094bb7f544854a324743c
Akshay <akshay@Akshays-MacBook-Pro.local>
2026-07-02 18:36:00 +0100

    Initial Spring Boot Maven project

---
+++ src/main/java/upi_payment_without_internet/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package upi_payment_without_internet.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
